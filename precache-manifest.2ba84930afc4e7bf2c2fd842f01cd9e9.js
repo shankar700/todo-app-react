d09af12266ab6632ffcbda1bbb2df7f5b21cb8e9
Shankar <bhavanishankarbst@gmail.com>
2020-05-13 22:46:40 +0530

    Updates

---
+++ precache-manifest.2ba84930afc4e7bf2c2fd842f01cd9e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca022e27eb9c45cb5543b94e2825729",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "e4d21db96ae15b7b3f66",
+    "url": "/todo-app-react/static/css/main.8498b1a9.chunk.css"
+  },
+  {
+    "revision": "17b9f182fff52fcfbf18",
+    "url": "/todo-app-react/static/js/2.b7b71e77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app-react/static/js/2.b7b71e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4d21db96ae15b7b3f66",
+    "url": "/todo-app-react/static/js/main.acad5210.chunk.js"
+  },
+  {
+    "revision": "16522272677688520643",
+    "url": "/todo-app-react/static/js/runtime-main.5f2a6303.js"
+  }
+]);
